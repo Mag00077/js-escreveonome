142a73e51b0e2aac62913641875dbc3332ca4d22
Bianca <168863923+Mag00077@users.noreply.github.com>
2024-05-07 23:17:06 -0300

    adicionando arquivo JS e CSS

---
+++ skets.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  function draw() {
+   stoker ("blue");
+    fill("red");
+    
+    1f(mou(seIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    {
+      
+      {
+        
